4df3502125234cefea0ed3e3d7700db9b069252e
Kent Yao <yaooqinn@hotmail.com>
2018-11-05 15:52:43 +0800

    #NESPARK-145 Add spark.driver.credential.updater.on to disable spark driver's credential updater

--- resource-managers/yarn/src/main/scala/org/apache/spark/scheduler/cluster/YarnClientSchedulerBackend.scala
+++ resource-managers/yarn/src/main/scala/org/apache/spark/scheduler/cluster/YarnClientSchedulerBackend.scala
@@ -65,7 +65,8 @@ private[spark] class YarnClientSchedulerBackend(
     // SPARK-8851: In yarn-client mode, the AM still does the credentials refresh. The driver
     // reads the credentials from HDFS, just like the executors and updates its own credentials
     // cache.
-    if (conf.contains("spark.yarn.credentials.file")) {
+    if (conf.contains("spark.yarn.credentials.file") &&
+      conf.getBoolean("spark.driver.credential.updater.on", defaultValue = true)) {
       YarnSparkHadoopUtil.startCredentialUpdater(conf)
     }
     monitorThread = asyncMonitorApplication()
